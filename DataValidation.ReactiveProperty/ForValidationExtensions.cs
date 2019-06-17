dd17a5083ad9a73ea3af46ee0e5fe71109a46961
NJ00 <nj00@outlook.com>
2019-06-17 23:11:54 +0900

    全エラーを返すヘルパメソッド追加

--- DataValidation.ReactiveProperty/ForValidationExtensions.cs
+++ DataValidation.ReactiveProperty/ForValidationExtensions.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reactive.Linq;
 using System.Reflection;
 using System.Text;
 using Nekoni.DataValidation.Validator;
@@ -40,13 +42,44 @@ namespace Nekoni.DataValidation.ReactiveProperty
                 // MethodInfoの場合はこう？
                 // https://ja.stackoverflow.com/questions/12801/c-type%E5%9E%8B%E3%81%A7%E6%8C%87%E5%AE%9A%E3%81%97%E3%81%9F%E5%9E%8B%E3%81%AB%E5%8B%95%E7%9A%84%E3%82%AD%E3%83%A3%E3%82%B9%E3%83%88%E3%81%99%E3%82%8B%E3%81%AB%E3%81%AF
                 var typeParameter = type.GetGenericArguments()[0];
-                var methodInfo = SetValidationMethodInfos.First(_=> 
+                var methodInfo = SetValidationMethodInfos.First(_ =>
                         _.GetParameters()[1].ParameterType == typeof(PropertyInfo));
                 var genericMethod = methodInfo.MakeGenericMethod(new[] { typeParameter });
                 genericMethod.Invoke(null, new[] { prop.Value, context.GetTargetPropInfo(prop.Key) });
             }
 
             return context;
+        }
+
+        /// <summary>
+        /// 全ReactivePropertyの検証エラーを返すヘルパ
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public static IObservable<IEnumerable<ValidationResult>> GetAllErrorsObservable(this ForValidation context)
+        {
+            var targets = context.GetTargetPropValues().Where(_ =>
+            {
+                var type = _.Value.GetType();
+                if (!type.IsGenericType) return false;
+
+                var rpType = type.GetGenericTypeDefinition();
+                if (rpType != typeof(ReactiveProperty<>)) return false;
+
+                return true;
+            })
+            .Select(_ =>
+            {
+                var type = _.Value.GetType();
+                var prop = type.GetProperty("ObserveErrorChanged");
+                var observable = prop.GetValue(_.Value) as IObservable<System.Collections.IEnumerable>;
+                if (observable == null) return null;
+                return observable.Select(e => e?.OfType<ValidationResult>());
+            })
+            .Where(_ => _ != null);
+
+            return Observable.CombineLatest(targets)
+                .Select(_ => _.Where(e => e != null).SelectMany(e => e));
         }
     }
 }
